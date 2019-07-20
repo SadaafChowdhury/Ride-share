9ca79f5bbc9b74ec4aafb73d55bfb3a7140718dc
SadaafChowdhury <c.sadaaf@gmail.com>
2019-07-20 13:43:12 -0700

    if else statement

--- Project Code/phpPractice.php
+++ Project Code/phpPractice.php
@@ -1,5 +1,11 @@
 <?php
-	$text = 'Hello World';
+	$number = 5;
+	
+	if(number=5){
+		echo 'The number is 5';
+	}
+	else{
+		echo 'The number is not 5';
+	}
 ?>
 
-<input type="text" name="<?php echo $text; ?>">
